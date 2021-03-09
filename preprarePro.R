02191cb8418767465213a06b817c49aacdd3807c
Pablo Velásquez <pablovmurcia@yahoo.com>
2021-03-08 21:32:44 -0500

    sasas

--- preprarePro.R
+++ preprarePro.R
@@ -5,8 +5,15 @@
 fileUrl <- "https://d3c33hcgiwev3.cloudfront.net/_6ee2a5c3100b9237616844a52883e240_intro_data_prob_project.Rmd?Expires=1614902400&Signature=iFkNCx-BPGyrbScCW6Oo93XrgZO0hlr44w8hQNjQANFeohzO0WlR4owFi04~vH7-hE~BMMjd6-y9oAYkV2uO2cuBd1ICCmvNVe893LAz023gDlkUO5SBVFhxU2zE8PW7HOueRnQs2vkAvCps0mNW9gzCCczt0VMPLruj5tdARL0_&Key-Pair-Id=APKAJLTNE6QMUY6HBC5A"
 download.file( fileUrl, destfile = "Project.Rmd", method = "curl")
 
+install.packages("viridis")
 library(dplyr)
 library(ggplot2)
+library(hrbrthemes)
+library(gmodels)
+library(gridExtra)
+library(viridis)
+library(viridisLite)
+
 # Download and load data
 fileUrl <- "https://d3c33hcgiwev3.cloudfront.net/4tiY2fqCQa-YmNn6gnGvzQ_1e7320c30a6f4b27894a54e2de50a805_brfss2013.RData?Expires=1614902400&Signature=cj3ZrzacK4iKJSar8jn3UrI1noO0MmmYrN4qf68nA9nMwVYyhWUd52vosLmnC4p4ti9tbsJAdShV~QsJHE2cnEgzxspWk2YeYaBYW6SZq-86MpZd4VgyY72bZ~OnHB0UFxrHqmZQpD8JiNM9w-m61nj9tFq3hDGgOrt78xczoIY_&Key-Pair-Id=APKAJLTNE6QMUY6HBC5A"
 download.file( fileUrl, destfile = "brfss2013.gz", method = "curl")
@@ -16,56 +23,6 @@ load("brfss2013.RData")
 
 
 # Understand data
-
-str(brfss2013)
-
-#  Some interesting variables
-
-# Year and months
-unique(brfss2013$iyear)
-table(brfss2013$iyear)
-unique(brfss2013$fmonth)
-table(brfss2013$fmonth)
-
-brfss2013 <- brfss2013 %>% mutate(day = paste(fmonth, iday, sep = " "))
-unique(brfss2013$day)
-grep("NA",brfss2013$day, value = TRUE )
-sort(table(brfss2013$day))
-
-# Satisfaction with life ------ One response variable
-unique(brfss2013$lsatisfy)
-table(brfss2013$lsatisfy, useNA = "ifany")
-
-# Time spent sleeping
-unique(brfss2013$sleptim1)
-table(brfss2013$sleptim1)
-
-# Questionnaire version
-unique(brfss2013$qstver)
-table(brfss2013$qstver)
-
-
-# Numbers od days mental health not good
-class(brfss2013$menthlth)
-unique(brfss2013$menthlth)
-table(brfss2013$menthlth)
-
-# sleep pattern
-class(brfss2013$sleptim1)
-unique(brfss2013$sleptim1)
-table(brfss2013$sleptim1)
-
-# Body mass index
-
-table(brfss2013$X_bmi5, useNA = "ifany")
-
-# Income categories
-
-table(brfss2013$X_incomg, useNA = "ifany")
-
-# Vegetables and fruit
-
-table(brfss2013$vegeda1_, useNA = "ifany")
 
 
 #### Consumption vegetables and fruit reduce blood pressure
@@ -87,7 +44,7 @@ noNA[names(noNA) == "lsatisfy"]
 ##### first question - ¿relationship between eating food and vegetables and good health?
 
 # create variable
-brfss2013 <- brfss2013 %>% 
+brfss2013Food <- brfss2013 %>% 
         filter(fruitju1 %in% (301:399) &
                        fruit1 %in% (301:399) &
                        fvgreen %in% (301:399) &
@@ -97,50 +54,124 @@ brfss2013 <- brfss2013 %>%
                        fruitju1 + fruit1 + fvgreen + fvorang + vegetab1-1500) %>%
         mutate(averageDayFruitVeg = totFruitVegetables/30.4167)
 
-max(brfss2013$averageDayFruitVeg)
 
-brfss2013 <- brfss2013 %>%
+brfss2013Food <- brfss2013Food %>%
         mutate(fruitVegeGroups = 
-                       cut(brfss2013v$averageDayFruitVeg,breaks = c(0,1,3,16.27395)))
+                       cut(brfss2013Food$averageDayFruitVeg,breaks = c(0,1,3,16.27395)))
 
-brfss2013 %>% select(averageDayFruitVeg, fruitVegeGroups)
-
-levels(brfss2013$fruitVegeGroups) <- c("less than 1 fruit or vegetable per day", 
-                                       "1 to 3 fruit or vegetable per day",
-                                       "more than 3 fruit or vegetable per day")
-
-table(brfss2013$X_r)
-table(brfss2013$X_rfhlth)
+levels(brfss2013Food$fruitVegeGroups) <- c("Less than 1 fruit/vegetable per day", 
+                                       "From 1 to 3 fruit/vegetable per day",
+                                       "More than 3 fruit/vegetable per day")
 
 
-install.packages("gmodels")
-library(gmodels)
-
-
-crosstable <- CrossTable(brfss2013$X_rfhlth, brfss2013$fruitVegeGroups)
+crosstable <- CrossTable(brfss2013Food$X_rfhlth, brfss2013Food$fruitVegeGroups)
 crosstable1 <- as.data.frame(crosstable[["prop.col"]])
 
-ggplot(crosstable1, aes(fill = crosstable1$x ,y = crosstable1$Freq , x =crosstable1$y)) + 
+ggplot(crosstable1, aes(fill = x ,y = Freq , x = y)) + 
         geom_bar(position ="stack", stat = "identity")
 
 
 ##### Second question - ¿There are possible coufounders to the relationship?
 
+## income
 
-
-crosstable <- CrossTable(brfss2013$fruitVegeGroups, brfss2013$X_incomg)
+# income vs vege and Fruit
+crosstable <- CrossTable(brfss2013Food$fruitVegeGroups, brfss2013Food$X_incomg)
 crosstable1 <- as.data.frame(crosstable[["prop.col"]])
-
-
 ggplot(crosstable1, aes(fill = crosstable1$x ,y = crosstable1$Freq , x =crosstable1$y)) + 
         geom_bar(position ="stack", stat = "identity")
 
+# income vs health
+
+crosstable <- CrossTable(brfss2013Food$X_rfhlth, brfss2013Food$X_incomg)
+crosstable1 <- as.data.frame(crosstable[["prop.col"]])
+ggplot(crosstable1, aes(fill = crosstable1$x ,y = crosstable1$Freq , x =crosstable1$y)) + 
+        geom_bar(position ="stack", stat = "identity")
+
+## exercise
+
+# exercise vs vege and fruit
+brfss2013FoodExce <- brfss2013Food %>% 
+        filter(exeroft1 %in% (201:299)) %>% 
+        mutate(ExerciPerMonth = 
+                       exeroft1 - 200 )
+# exercise vs health
+p <- ggplot(brfss2013FoodExce, aes(x=ExerciPerMonth, y=averageDayFruitVeg)) +
+        geom_point(alpha = 1/5, size = 1.5) +
+        geom_smooth(method=lm , color="red", fill="#69b3a2", se=TRUE) 
 
 
+crosstable <- CrossTable(brfss2013Food$X_rfhlth, brfss2013Food$exerany2)
+crosstable1 <- as.data.frame(crosstable[["prop.col"]])
+ggplot(crosstable1, aes(fill = crosstable1$x ,y = crosstable1$Freq , x =crosstable1$y)) + 
+        geom_bar(position ="stack", stat = "identity")
 
+##### third question - ¿if we control confounders, the relationship between vega and fruit
+# and health still prevails?
 
-names(table(brfss2013$lsatisfy, useNA = "ifany"))
+# Income
 
+brfss2013Foodless15 <- brfss2013Food %>% filter(X_incomg == "Less than $15,000") 
+crosstable <- CrossTable(brfss2013Foodless15$X_rfhlth, brfss2013Foodless15$fruitVegeGroups)
+crosstable1 <- as.data.frame(crosstable[["prop.col"]])
+names(crosstable1) <- c("Health_Status", "y", "Freq")
+p1 <- ggplot(crosstable1, aes(fill = Health_Status ,y = Freq , x = y)) + 
+        geom_bar(position ="stack", stat = "identity") +
+        ggtitle("Less than $15,000") + 
+        ylab("Relative Frequency") + 
+        scale_fill_manual(values=c("steelblue2", "indianred2")) + 
+        theme(axis.text.x=element_text(size= 6.3), axis.title.x=element_blank())
 
+brfss2013Food15to25 <- brfss2013Food %>% filter(X_incomg == "$15,000 to less than $25,000") 
+crosstable <- CrossTable(brfss2013Food15to25$X_rfhlth, brfss2013Food15to25$fruitVegeGroups)
+crosstable1 <- as.data.frame(crosstable[["prop.col"]])
+names(crosstable1) <- c("Health_Status", "y", "Freq")
+p2 <-ggplot(crosstable1, aes(fill = Health_Status ,y = Freq , x = y)) + 
+        geom_bar(position ="stack", stat = "identity") +
+        ggtitle("$15,000 to less than $25,000") +
+        ylab("Relative Frequency") + 
+        scale_fill_manual(values=c("steelblue2", "indianred2")) + 
+        theme(axis.text.x=element_text(size=6.3), axis.title.x=element_blank())       
 
+brfss2013Food125to35 <- brfss2013Food %>% filter(X_incomg == "$25,000 to less than $35,000") 
+crosstable <- CrossTable(brfss2013Food125to35$X_rfhlth, brfss2013Food125to35$fruitVegeGroups)
+crosstable1 <- as.data.frame(crosstable[["prop.col"]])
+names(crosstable1) <- c("Health_Status", "y", "Freq")
+p3 <-ggplot(crosstable1, aes(fill = Health_Status ,y = Freq , x = y)) + 
+        geom_bar(position ="stack", stat = "identity") +
+        ggtitle("$25,000 to less than $35,000") +
+        ylab("Relative Frequency") + 
+        scale_fill_manual(values=c("steelblue2", "indianred2")) + 
+        theme(axis.text.x=element_text(size=6.3), axis.title.x=element_blank())          
+
+brfss2013Food135to50 <- brfss2013Food %>% filter(X_incomg == "$35,000 to less than $50,000") 
+crosstable <- CrossTable(brfss2013Food135to50$X_rfhlth, brfss2013Food135to50$fruitVegeGroups)
+crosstable1 <- as.data.frame(crosstable[["prop.col"]])
+names(crosstable1) <- c("Health_Status", "y", "Freq")
+p4 <-ggplot(crosstable1, aes(fill = Health_Status ,y = Freq , x = y)) + 
+        geom_bar(position ="stack", stat = "identity") +
+        ggtitle("$35,000 to less than $50,000") +
+        ylab("Relative Frequency") + 
+        scale_fill_manual(values=c("steelblue2", "indianred2")) + 
+        theme(axis.text.x=element_text(size=6.3), axis.title.x=element_blank())  
+
+brfss2013Food50 <- brfss2013Food %>% filter(X_incomg == "$50,000 or more") 
+crosstable <- CrossTable(brfss2013Food50$X_rfhlth, brfss2013Food50$fruitVegeGroups)
+crosstable1 <- as.data.frame(crosstable[["prop.col"]])
+names(crosstable1) <- c("Health_Status", "y", "Freq")
+p5 <-ggplot(crosstable1, aes(fill = Health_Status ,y = Freq , x = y)) + 
+        geom_bar(position ="stack", stat = "identity") +
+        ggtitle("$50,000 or more") +
+        ylab("Relative Frequency") + 
+        scale_fill_manual(values=c("steelblue2", "indianred2")) + 
+        theme(axis.text.x=element_text(size=6.3), axis.title.x=element_blank())  
+
+grid.arrange(p1,p2,p3,p4,p5, ncol=2)
+
+table(brfss2013Food$X_incomg)
+
+# Income -- neh
+p <- ggplot(brfss2013FoodExce, aes(x=ExerciPerMonth, y=averageDayFruitVeg, colour =X_rfhlth)) +
+  geom_point( size = 1.5) +
+  geom_smooth(method=lm , color="red", fill="#69b3a2", se=TRUE) 
 
